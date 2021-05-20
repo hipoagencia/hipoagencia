6a89c24507469fb84133dd0d93ea30fdd964979b
hipoagencia <falecom@hipoagencia.com.br>
2021-05-20 16:55:27 -0300

    Ajuste Login

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -26,37 +26,20 @@
         <div class="row justify-content-center">
             <div class="col-md-8 col-lg-6 col-xl-5">
                 <div class="card overflow-hidden">
-                    <div class="bg-primary bg-soft">
-                        <div class="row">
-                            <div class="col-7">
-                                <div class="text-primary p-4">
-                                    <h5 class="text-primary">Welcome Back !</h5>
-                                    <p>Sign in to continue to Skote.</p>
-                                </div>
-                            </div>
-                            <div class="col-5 align-self-end">
-                                <img src="{{ asset('backend/assets/images/profile-img.png') }}" alt="" class="img-fluid">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body pt-0">
-                        <div class="auth-logo">
-                            <a href="index.html" class="auth-logo-light">
-                                <div class="avatar-md profile-user-wid mb-4">
-                                            <span class="avatar-title rounded-circle bg-light">
-                                                <img src="{{ url('backend/assets/images/logo-light.svg') }}" alt="" class="rounded-circle" height="34">
-                                            </span>
-                                </div>
-                            </a>
-
-                            <a href="index.html" class="auth-logo-dark">
-                                <div class="avatar-md profile-user-wid mb-4">
-                                            <span class="avatar-title rounded-circle bg-light">
-                                                <img src="{{ url('backend/assets/images/logo.svg') }}" alt="" class="rounded-circle" height="34">
-                                            </span>
-                                </div>
-                            </a>
-                        </div>
+{{--                    <div class="bg-primary bg-soft">--}}
+{{--                        <div class="row">--}}
+{{--                            <div class="col-7">--}}
+{{--                                <div class="text-primary p-4">--}}
+{{--                                    <h5 class="text-primary">Seja bem vindo!</h5>--}}
+{{--                                    <p>Efetue o login para continuar</p>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <div class="col-5 align-self-end">--}}
+{{--                                <img src="{{ asset('backend/assets/images/profile-img.png') }}" alt="" class="img-fluid">--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+                    <div class="card-body pt-5">
                         <div class="p-2">
                             <form class="form-horizontal" action="{{ route('login.do') }}" method="POST" autocomplete="false">
                                 @csrf
@@ -85,17 +68,17 @@
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember">
                                     <label class="form-check-label" for="remember-check">
-                                        Remember me
+                                        Lembre-me
                                     </label>
                                 </div>
 
                                 <div class="mt-3 d-grid">
-                                    <button class="btn btn-primary waves-effect waves-light" type="submit">Log In</button>
+                                    <button class="btn btn-primary waves-effect waves-light" type="submit">Entrar</button>
                                 </div>
 
 
                                 <div class="mt-4 text-center">
-                                    <a href="#" class="text-muted"><i class="mdi mdi-lock me-1"></i> Forgot your password?</a>
+                                    <a href="#" class="text-muted"><i class="mdi mdi-lock me-1"></i> Esqueceu a senha?</a>
                                 </div>
                             </form>
                         </div>
@@ -105,7 +88,8 @@
                 <div class="mt-5 text-center">
 
                     <div>
-                        <p>© <script>document.write(new Date().getFullYear())</script> Skote. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
+                        <p>© <script>document.write(new Date().getFullYear())</script> {{ env('app_name') }}. Desenvolvido com <i class="mdi mdi-heart text-danger"></i> por
+                            <a href="https://hipoagencia.com.br" target="_blank">Hipo Agência</a></p>
                     </div>
                 </div>
 
