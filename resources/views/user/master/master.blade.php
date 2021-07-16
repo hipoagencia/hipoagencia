93618fc0d399ffc297071834401f50428bc33d8c
hipoagencia <falecom@hipoagencia.com.br>
2021-07-15 22:06:32 -0300

    Tentativa de ajuste livewire

--- resources/views/user/master/master.blade.php
+++ resources/views/user/master/master.blade.php
@@ -15,7 +15,8 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
 
-    @livewireStyles
+
+@livewireStyles
 
     @hasSection('css')
         @yield('css')
@@ -44,6 +45,7 @@
 <script src="{{ url(asset('backend/assets/libs/jquery/jquery.min.js')) }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 
 @livewireScripts
 
