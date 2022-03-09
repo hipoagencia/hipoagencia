f57aab46636ff76c45e13a8b532ae2f9f4b8f1f5
hipoagencia <falecom@hipoagencia.com.br>
2022-03-09 11:09:46 -0300

    ajuste ssl favicon 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,9 +29,9 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrap();
         Schema::defaultStringLength(191);
 
-        if($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+//        if($this->app->environment('production')) {
+//            URL::forceScheme('https');
+//        }
 
     }
 }
