1bb189e3fe6c3dcc8bb3a8df7259a63cbed29ad3
hipoagencia <falecom@hipoagencia.com.br>
2022-06-28 10:52:01 -0300

    email

---
+++ resources/views/web/thanks.blade.php
@@ -0,0 +1,34 @@
+@extends('web.master.master')
+
+@section('content')
+
+
+    <section class="half-section wow animate__fadeIn page-title-colorful" data-backgroundcolor="#1c1c1c,#363636,#1c1c1c,#363636">
+        <div class="container">
+            <div class="d-flex flex-column flex-md-row justify-content-end extra-small-screen">
+                <div class="w-100 w-md-50 align-self-end page-title-extra-small">
+                    <h1 class="alt-font text-white position-relative padding-85px-left padding-15px-right sm-padding-55px-left margin-30px-bottom sm-margin-20px-bottom"><span class="page-title-separator-line bg-white w-70px sm-w-40px"></span>Contato</h1>
+                </div>
+                <div class="w-100 w-md-50 align-self-end">
+                    <h4 class="alt-font text-white font-weight-500 margin-30px-bottom">Obrigado por sua mensagem!</h4>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="p-0 cover-background wow animate__fadeIn">
+        <div class="container">
+            <div class="row align-items-stretch justify-content-center full-screen">
+                <div class="col-12 col-xl-6 col-lg-7 col-md-8 text-center d-flex align-items-center justify-content-center flex-column">
+                    <h6 class="alt-font font-weight-600 letter-spacing-minus-1px margin-10px-bottom text-uppercase">Agora é só aguardar<br>que já vamos te chamar.</h6>
+                    <h1 class="alt-font text-extra-big font-weight-700 letter-spacing-minus-5px text-extra-dark-gray margin-6-rem-bottom md-margin-4-rem-bottom">:D</h1>
+                    <span class="alt-font font-weight-500 text-extra-dark-gray d-block margin-20px-bottom">Caso tenha pressa, nos chame pelo WhatsApp no botão ao lado!</span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+@endsection
+
